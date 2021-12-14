b631d0d09e863c69883e427a1575163333851803
Antoine CARRE <79215772+Antoine-Carre@users.noreply.github.com>
2021-12-14 11:13:17 +0100

    Update pilotage_maj_hiver_2021.py

--- pilotage_maj_hiver_2021.py
+++ pilotage_maj_hiver_2021.py
@@ -246,8 +246,10 @@ if categorie_2 == 'Structures':
                 fig3 = px.bar(table_2, x="created_at", y=["les acteurs"], color_discrete_sequence= ['#3E3A71', '#2896A0'], title="Nombre de fiches mise à jour par jour et status") 
             elif not "les acteurs" in list(table_2.columns):
                 fig3 = px.bar(table_2, x="created_at", y=["l'équipe Soliguide"], color_discrete_sequence= ['#3E3A71', '#2896A0'], title="Nombre de fiches mise à jour par jour et status") 
-            else:
+            elif not "l'équipe territoriale" in list(table_2.columns):
                 fig3 = px.bar(table_2, x="created_at", y=["l'équipe Soliguide", "les acteurs"], color_discrete_sequence= ['#3E3A71', '#2896A0'], title="Nombre de fiches mise à jour par jour et status") 
+            else:
+                fig3 = px.bar(table_2, x="created_at", y=["l'équipe Soliguide", "les acteurs", "l'équipe territoriale"], color_discrete_sequence= ['#3E3A71', '#2896A0'], title="Nombre de fiches mise à jour par jour et status") 
 
             fig3.update_traces(hovertemplate = "Date de la mise à jour : le %{x}<br>Nbre de fiches: %{value}")
             fig3.update_layout(xaxis=dict(tickformat="%d %B %Y"), xaxis_title="", yaxis_title="Nombre de fiches",)
