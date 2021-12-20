305bea779a019cbfd428f3629f15217463c5d584
Antoine CARRE <79215772+Antoine-Carre@users.noreply.github.com>
2021-12-20 16:17:07 +0100

    Update pilotage_maj_hiver_2021.py

--- pilotage_maj_hiver_2021.py
+++ pilotage_maj_hiver_2021.py
@@ -3,7 +3,9 @@ import pandas as pd
 import numpy as np
 from datetime import timedelta
 import datetime
+from datetime import datetime
 import plotly.express as px
+import plotly.graph_objects as go
 import streamlit.components.v1 as components
 
 @st.cache(allow_output_mutation=True)
@@ -23,7 +25,7 @@ st.set_page_config(page_title="Soliguide - Mise à jour hiver 2021-2022",
 ############# 
 st.sidebar.image("https://soliguide.fr/assets/images/logo.png", use_column_width=True)
 st.sidebar.title('Soliguide 2021')
-st.sidebar.subheader('Mise à jour été')
+st.sidebar.subheader('Mise à jour hiver')
 
 categorie = st.sidebar.selectbox("Choisissez votre territoire :", ("France", "Alpes-Maritimes (06)",
                                             "Ardèche (07)","Bouche-du-Rhône (13)","Cantal (15)",
@@ -215,10 +217,22 @@ if categorie_2 == 'Structures':
             table.reset_index(inplace=True)
         
         # Nbre de fiches màj par jour
+                       
+            fig_2 = go.Figure(data=[
+                go.Bar(name="Fiches actualisées", x=table['date'], y=table["Fiches actualisées"], marker_color='#7201a8'),
+                go.Bar(name="Fiches à mettre à jour", x=table['date'], y=table["Fiches à mettre à jour"], marker_color='#d8576b')
+            ])
+            # Change the bar mode
+            fig_2.update_layout(barmode='stack')
 
-            fig_2 = px.bar(table, x="date", y=["Fiches actualisées", "Fiches à mettre à jour"], color_discrete_sequence= [ '#7201a8', '#d8576b']) 
-            fig_2.update_traces(hovertemplate = "Date du dernier relevé des mises à jour : le %{x}<br>Nbre de fiches: %{value}")
             fig_2.update_layout(xaxis=dict(tickformat="%d %B %Y"), xaxis_title="", yaxis_title="Nombre de fiches",)
+            fig_2.update_traces(hovertemplate = "Date de la mise à jour : le %{x}<br>Nbre de fiches: %{value}")
+
+            dt_all = pd.date_range(start=table['date'].iloc[0],end=table['date'].iloc[-1])
+            dt_obs = [d.strftime("%Y-%m-%d") for d in pd.to_datetime(table['date'])]
+            dt_breaks = [d for d in dt_all.strftime("%Y-%m-%d").tolist() if not d in dt_obs]
+
+            fig_2.update_xaxes(rangebreaks=[dict(values=dt_breaks)])
 
             st.plotly_chart(fig_2, use_container_width=True)
 
