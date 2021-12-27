e65c690c45a988c27ab8413480d44dfadbdabb84
Antoine CARRE <79215772+Antoine-Carre@users.noreply.github.com>
2021-12-27 18:15:48 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
 mkdir -p ~/.streamlit/
 
-echo "[server]
-headless = true
-port = $PORT
-enableCORS = false
+echo "
+[general]
+email = \"antoine.carre@gmx.com\"
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
